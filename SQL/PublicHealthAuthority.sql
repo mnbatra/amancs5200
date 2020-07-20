f6843a6e31dc54014c5bd49972dfe5c0f31b6fe0
AB <yourbatra@gmail.com>
2020-07-20 11:26:26 +0530

    Adding Practicum-1 DBMSCS5200 Summer 2020 to my git

---
+++ SQL/PublicHealthAuthority.sql
@@ -0,0 +1,12 @@
+INSERT INTO PublicHealthAuthority(AuthorityID, AuthAddressID, Name, Jurisdiction) VALUES
+    (13100,1,'Mollis Lectus Pede Institute','EAST BOSTON'),
+    (13101,2,'Eleifend Ltd','BOSTON'),
+    (13102,3,'Cursus Integer Company','SOUTH BOSTON'),
+    (13103,4,'Quis Diam Pellentesque Consulting','BOSTON'),
+    (13104,5,'Non Foundation','SOUTH BOSTON'),
+    (13105,6,'Et Magna Praesent PC','EAST BOSTON'),
+    (13106,7,'Tristique Incorporated','EAST BOSTON'),
+    (13107,8,'Nunc Sed Corp.','EAST BOSTON'),
+    (13108,9,'Ipsum Phasellus Associates','BOSTON'),
+    (13109,10,'Ut Nec PC','DARMOUTH'),
+    (13110,11,'Eleifend Associates','BOSTON');
